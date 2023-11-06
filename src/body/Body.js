bd45b5d59cfdf87ca47272dabc8deef059d37a01
Ben Graham <59644205+bengraham-B@users.noreply.github.com>
2023-11-06 14:51:16 +0200

    fix the rerender problem

--- src/body/Body.js
+++ src/body/Body.js
@@ -1,44 +1,46 @@
 import React, {useState, useEffect} from 'react'
 import Money from './Money'
-import IPAddress from './IP_Address'
+// import IPAddress from './IP_Address'
 
 export default function Body() {
     const [details, setDetails] = useState()
     const [amount, setAmount] = useState()
     const [moneyArrayLS, setMoneyArrayLS] = useState([])
-    const [month, setMonth] = useState('')
+    // const [month, setMonth] = useState('')
 
     const [noTransactions, setNoTransactions] = useState(true)
 
     class MoneyClass {
-        constructor(details, amount, date){
+        constructor(details, amount){
             this.details = details
             this.amount = amount
-            this.date = date
+            this.date = new Date().toDateString()
+
+            this.month = this.getMonth()
+
         }
+        
+        getMonth(){
+            const monthArray = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+            const dateVar = new Date()
+            const month = monthArray[dateVar.getMonth()]
+            return month
+        }
+
+        
     }
 
     function getMoney() {
         if(localStorage.getItem('money') === null){
             setNoTransactions(true)
-            console.log("GGGGG")
         }
 
         else {
             try {
                 setNoTransactions(false); // Update the state
-                console.log("GGGGG");
-    
-                // Retrieve data from localStorage
                 const jsonData = JSON.parse(localStorage.getItem('money'));
-                console.log(jsonData);
-    
-                // Set the data to moneyArrayLS and log it
-                // jsonData.map(data => setMoneyArrayLS(data))
-                console.log(jsonData)
                 const newMoneyArray = []
                 jsonData.map(data => newMoneyArray.push(data))
-                console.log(newMoneyArray)
                 setMoneyArrayLS(newMoneyArray)
                 
             } catch (error) {
@@ -48,7 +50,6 @@ export default function Body() {
             
         }
     }
-    console.log(moneyArrayLS)
     
     
 
@@ -65,8 +66,7 @@ export default function Body() {
             else {
                 const jsonData = JSON.parse(localStorage.getItem('money'));
                 console.log(jsonData);
-                const date = new Date().toDateString()
-                jsonData.push(new MoneyClass(details, amount, date));
+                jsonData.push(new MoneyClass(details, amount));
                 localStorage.setItem('money', JSON.stringify(jsonData));
                 console.log(moneyArrayLS)
             }
@@ -75,20 +75,16 @@ export default function Body() {
             console.log(error)
         }
 
-        setAmount('')
-        setDetails('')
+        // setAmount('')
+        // setDetails('')
 
 
     }
-
-    console.log(moneyArrayLS)
 
 
     useEffect(() => {
 
         getMoney()
-    setMonth("November")
-
 
     }, [handleClick])
     
@@ -132,7 +128,7 @@ export default function Body() {
                     <div>
                         <h1 className='text-4xl text-blue-700'>Transaction Histroy</h1>
                         <p className="text-center text-xl text-blue-600">Transactions: {moneyArrayLS.length}</p>
-                        <p className="text-center text-xl text-blue-600">{month}</p> 
+                        <p className="text-center text-xl text-blue-600">{'month'}</p> 
 
                     </div>
                     }
@@ -142,7 +138,7 @@ export default function Body() {
                    moneyArrayLS && moneyArrayLS.map((money, index) => (
 
 
-                    <Money key={index} details={money.details} amount={money.amount} date={money.date} monthProp={month}/>
+                    <Money key={index} details={money.details} amount={money.amount} date={money.date} monthProp={'month'}/>
 
 
 
